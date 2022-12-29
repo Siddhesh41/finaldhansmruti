39bbc01d666e5d5babce2194856ddfeaabc07a9e
Siddhesh Borse <90822039+Siddhesh41@users.noreply.github.com>
2022-12-29 23:09:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+
+
+
+window.addEventListener("scroll",function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY>0);
+});
+
+var initialSrc = "images/image2.png";
+var scrollSrc = "images/image1.png";
+
+$(window).scroll(function() {
+var value = $(this).scrollTop();
+if (value > 0)
+    $(".logo").attr("src", scrollSrc);
+else
+    $(".logo").attr("src", initialSrc);
+});
+
+// 2nd file
+
+const myslide = document.querySelectorAll('.myslide'),
+	  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 4000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 4000);
+}
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	for(i = 0;i<dot.length;i++) {
+		dot[i].className = dot[i].className.replace(' active', '');
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+	dot[counter - 1].className += " active";
+}
